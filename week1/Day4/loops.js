021e10e33cce1887cbb4fadbb1221ab579563360
emanhashim <emanhashim42@gmail.com>
2019-06-06 11:57:48 -0700

    js loops

---
+++ week1/Day4/loops.js
@@ -0,0 +1,20 @@
+// var Book={
+//   Title: "Dertogada",
+//   Author: "Yismake Worku",
+//   ReleaseDate: "2000",
+// }
+// console.log(Book.Title)
+// console.log(Book("Title"))
+ 
+// var foods =["Rice","Avocado","Doro Wet"]
+// // console.log(foods)
+
+// for (var i=0; i <= foods.length; i++) {
+//   console.log(foods[i])
+// }
+
+var count = 1;
+  while (count <= 10){
+  console.log(count)
+    count += 1
+}
