a1c27f7e2f0323f7ec3072d26cbe814e683151a2
emanhashim <emanhashim42@gmail.com>
2019-06-07 00:22:16 -0700

    buzz

---
+++ week1/Day5/fuzzbuzz.js
@@ -0,0 +1,37 @@
+ // var num = i
+
+ for (var i = 1; i <= 100; i++){ 
+   if (i % 3 == 0 && i % 5 == 0) {
+ console.log("fizz buzz") 
+   }
+ else if (i % 3 == 0){
+   
+  console.log("fizz")
+ }
+ 
+ else if(i % 5 == 0){
+console.log("buzz")
+ }
+ else  {
+ console.log(i)}
+   
+ }
+ 
+ //  for(var num = 1; num<=100; num++)           
+ //  }
+ // }
+ // }
+ // }
+ //  function numbers(num){
+ //    if(num%3===0){
+ //  return("fizz")
+ //  {
+ //  elseif(num%5===0){
+ // return("buzz")
+ //  {
+ //   elseif(num%3&&5===0)
+ //  {
+ // return("fizz buzz")
+ //   }
+ //  }
+ //    numbers(num)
